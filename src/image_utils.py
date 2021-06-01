8a81be43fbeb7942020f2b4a0c9b2cad6c59df36
Aayushi Agarwal <39083595+Aayushi0008@users.noreply.github.com>
2021-06-01 19:51:23 +0530

    Update image_utils.py

--- src/image_utils.py
+++ src/image_utils.py
@@ -3,12 +3,10 @@ import tensorflow as tf
 from PIL import Image
 from scipy import fftpack
 
-
 def dct(array):
     array = fftpack.dct(array, type=2, norm="ortho", axis=0)
     array = fftpack.dct(array, type=2, norm="ortho", axis=1)
     return array
-
 
 def fft(array):
     array = np.transpose(array, axes=[2, 0, 1])
@@ -16,16 +14,13 @@ def fft(array):
     array = np.transpose(array, axes=[1, 2, 0])
     return array
 
-
 def load_image(path):
     x = Image.open(path)
     return np.asarray(x)
 
-
 def normalize(image, mean, std):
     image = (image - mean) / std
     return image
-
 
 def scale_image(image):
     image /= 127.5
